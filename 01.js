c2c5fb1b7907813741dec40a3a12dc4cc24366c7
david <davit.kardava13b@geolab.edu.ge>
2020-05-22 17:23:09 +0400

    done

---
+++ 01.js
@@ -0,0 +1,9 @@
+let i = 1;
+let sum = 0;
+while (i < 10) {
+    if (i % 2 != 0) {
+        sum = sum = i;
+    }
+    i = i + 1;
+}
+ console.log(sum);
---
+++ 02.js
@@ -0,0 +1,5 @@
+let sum = 0;
+for (let i = 0; i < 50; i += 3) {
+    sum = sum + i;
+}
+console.log(sum);
